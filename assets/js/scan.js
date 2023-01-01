23d5c72e5ee91d7c76bf140a54a5f1bb91778d74
Saurabh Tiwari <86095513+SaurabhTiwari4093@users.noreply.github.com>
2023-01-01 13:42:15 +0530

    added qr scan api

--- assets/js/scan.js
+++ assets/js/scan.js
@@ -9,6 +9,42 @@ function docReady(fn) {
     }
 }
 
+const qrScanApi = async (data) => {
+    if (data.eventName === '') {
+        alert("Please choose eventName")
+    }
+    else {
+        try {
+            const formData = {
+                eventName: data.eventName,
+                studentEmail: data.studentEmail,
+            }
+            const requestOptions = {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData)
+            }
+            const url = `https://becon-edc.azurewebsites.net/api/event/qrScan`;
+            await fetch(url, requestOptions)
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data)
+                    if (data.status === 200) {
+                        alert(data.message);
+                    }
+                    else {
+                        alert(data.message);
+                    }
+                })
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+}
+
 docReady(function () {
     var resultContainer = document.getElementById('qr-reader-results');
     var lastResult, countResults = 0;
@@ -16,8 +52,14 @@ docReady(function () {
         if (decodedText !== lastResult) {
             ++countResults;
             lastResult = decodedText;
+
             // Handle on success condition with the decoded message.
             console.log(`Scan result ${decodedText}`, decodedResult);
+            const requestData = {
+                eventName: "InaugrationSession",
+                studentEmail: "saurabhtiwari4093@gmail.com"
+            }
+            qrScanApi(requestData);
         }
     }
 
